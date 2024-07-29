0e7f5a616aca2478ebc1366c51e3969446771e8b
Nick Lombardo <nlombardo@ddn.com>
2024-07-29 09:44:13 -0400

    endabled rust LSP

--- init.lua
+++ init.lua
@@ -490,7 +490,7 @@ require('lazy').setup({
         bashls = {},
         -- gopls = {},
         pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
